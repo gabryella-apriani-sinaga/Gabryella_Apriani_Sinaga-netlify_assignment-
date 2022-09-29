e58e2c2f5662cb8a96d36d5da6e09bd958ef38f6
Gabryella Apriani Sinaga <82702159+gabryella-apriani-sinaga@users.noreply.github.com>
2022-09-29 12:58:41 +0700

    upload file

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+// File to enable tailwindcss intellisense
+module.exports = {
+  theme: {
+    screens: {
+      tablet: "320px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1080px",
+      // => @media (min-width: 1024px) { ... }
+    },
+  },
+};
